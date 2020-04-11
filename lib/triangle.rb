1153d6090f5af71fb7f929d8975f9b2d610d84a2
Heather Moreira <moreiraheather@gmail.com>
2020-04-11 01:01:22 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,56 @@
+require 'pry' 
+
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c 
+  attr_reader :sides  
+  
+  def initialize(a, b, c) 
+    @a = a 
+    @b = b 
+    @c = c 
+    @sides = [a, b, c]
+  end 
+  
+  def valid? 
+    self.sides.all?{|side| side > 0} && triangle_inequality? 
+  end 
+  
+  def triangle_inequality?
+    check_1 = @a + @b > @c
+    check_2 = @a + @c > @b 
+    check_3 = @b + @c > @a 
+    check_1 && check_2 && check_3 
+  end 
+
+  
+  def equilateral?
+    @a == @b && @b == @c
+  end 
+    
+  def isosceles? 
+    @a == @b || @a == @c || @b == @c 
+  end 
+
+  def scalene?
+    @a != @b && @a != @c && @b != @c 
+  end 
+    
+  
+  def kind 
+    if !(self.valid?) 
+      raise TriangleError
+    elsif equilateral? 
+      :equilateral 
+    elsif isosceles? 
+      :isosceles 
+    else scalene? 
+      :scalene 
+    end 
+  end 
+
+  class TriangleError < StandardError 
+    def message 
+      "this is not a valid triangle."
+    end 
+  end 
 end
